508c8cad74abdf34fed72388141447aa24ad3d86
Muhammad Hamza <160265626+mdhamzarj@users.noreply.github.com>
2024-07-20 12:19:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,134 @@
+var name = prompt ("Your name?");
+  alert ("Welcome to the Quiz " + name);
+
+//   question #1
+
+     var q1 = prompt("Inside which HTML element do we put the JavaScript?");
+if (q1 === "<script>"){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+//   question #2
+
+     var q2 = prompt("Where is the correct place to insert a JavaScript?");
+     if (q2 === "body"){
+        alert("Correct !");
+     }
+     else 
+          {
+              alert("ohh sorry!");
+          }
+
+//   question #3
+
+
+var q3 = prompt("How can you add a comment in a JavaScript?");
+if (q3 === "// comment"){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+
+
+     //   question #4
+
+
+var q4 = prompt("How do you add a function in Javascript?");
+if (q4 === "function myfunction()"){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+
+     
+     //   question #5
+
+
+var q5 = prompt("What is the correct syntax for referring to an external script called a.js?");
+if (q5 === "<script src= a.js>"){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+
+      //   question #6
+
+
+var q6 = prompt("How do round the number 7.25 to the nearest integer?");
+if (q6 === "Math.round (7.25)"){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+
+      //   question #7
+
+
+var q7 = prompt("Is the javascript same as java?");
+if (q7 === "no"){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+
+//   question #8
+
+
+var q8 = prompt("How do you declare a Javascript variables?");
+if (q8 === "var"){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+
+     //   question #9
+
+
+var q9 = prompt("Which operator is assign a value to a variable?");
+if (q9 === "="){
+   alert("Correct !");
+}
+else 
+     {
+         alert("ohh sorry!");
+     }
+
+ //   question #10
+
+
+ var q10 = prompt("Is Javascript case-sensitive?");
+ if (q10 === "yes"){
+    alert("Correct !");
+ }
+ else 
+      {
+          alert("ohh sorry!");
+
+      }
+
+
+alert("Congratulations! You Have Passed");
+
+
+
+
+
+
+
+
+
